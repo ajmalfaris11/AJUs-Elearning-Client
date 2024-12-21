15f7d747c9e405fef262afe1264e99fb6e7880d0
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-22 02:21:28 +0530

    feat: Integrate HeroSection and Header components into the main Page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import HeroSection from "./components/Route/Hero";
 
-interface Props{}
+interface Props {}
 
-
-const Page: FC <Props> = (props) => {
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
-      <h1>hello world</h1>
+      <Heading
+        title="AJUS E-Learning"
+        description="Elearning is a platform for students to learn and get help from teachers"
+        keywords="Programming, MERN, Redux, Machine Learning"
+      />
+
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+
+      <HeroSection />
     </div>
   );
 };
