eb3d29d0ade6e8515dcbef4f0d991bbdd4b449cd
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-24 12:11:25 +0530

    feat(auth-api): add authentication API endpoints
    - Added egister mutation to handle user registration with server interaction.
    - Implemented onQueryStarted to dispatch userRegistration on successful registration.
    - Added ctivation mutation for user account activation.
    - Enhanced error handling for server responses and HTTP errors.

---
+++ redux/features/auth/authApi.ts
@@ -0,0 +1,57 @@
+import { apiSlice } from "../api/apiSlice";
+import { userRegistration } from "./authSlice";
+
+type RegistrationResponse = {
+  message: string;
+  activationToken: string;
+};
+
+type RegistrationData = {
+  // name: string;
+  // email: string;
+  // password: string;
+};
+
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "registration",
+        method: "POST",
+        body: data,
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userRegistration({
+              token: result.data.activationToken,
+            })
+          );
+        } catch (error: any) {
+          if (error?.data) {
+            console.error("Server error:", error.data);
+          } else if (error?.status) {
+            console.error(`HTTP Error (Status: ${error.status}):`, error);
+          } else {
+            console.error("Unhandled error format:", error);
+          }
+        }
+      },
+    }),
+    activation: builder.mutation({  //
+      query: ({ activation_token, activation_code }) => ({
+        url: "activation-user",
+        method: "POST",
+        body: {
+          activation_token,
+          activation_code,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useActivationMutation } = authApi;
