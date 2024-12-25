c6d1b4d47c1f2ee8b86a464a50d724694b22cd42
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-26 04:44:46 +0530

    Added refresh token endpoint to apiSlice
    - Defined `refreshToken` endpoint using `builder.query` to handle the "refresh-token" request.
    - Set the HTTP method to GET and added `credentials: "include"` for secure cookie handling.
    - Exported `useRefreshTokenQuery` hook to be used for triggering the refresh token API request.

--- redux/features/api/apiSlice.ts
+++ redux/features/api/apiSlice.ts
@@ -3,7 +3,16 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_SERVER_URI }),
-    endpoints: (builder) => ({}),
-});
+    endpoints: (builder) => ({
+        refreshToken: builder.query({
+            query: (data) => ({
+                url: "refresh-token",
+                method: "GET",
+                credentials: "include" as const,
+            }),
+    
+        }),
+    }),
+}); 
 
-export const {} = apiSlice;
+export const {useRefreshTokenQuery} = apiSlice;
