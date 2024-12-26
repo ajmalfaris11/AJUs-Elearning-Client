85e392b80cafac3ab4f1cf677d35d83dae154f6e
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-12-27 04:06:39 +0530

    feat: add profile page with protected route and user details
    - Implements the profile page, accessible only to authenticated users using the `Protected` component.
    - Displays dynamic user-specific information, such as the user's name in the page title.
    - Integrates `Heading`, `Header`, and `Profile` components to structure the profile page layout.
    - Uses `useSelector` to fetch user details from the Redux store.
    - Manages state for the header's open/close status, active menu item, and selected route.

---
+++ app/profile/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+import React, { FC, useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import Profile from "../components/Profile/Profile";
+import { useSelector } from "react-redux";
+
+
+
+type Props = {};
+
+const page: FC<Props> = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+  const {user} = useSelector((state:any) => state.auth);
+
+
+  return (
+    <div>
+      <Protected>
+        <Heading
+          title={`${user.name}'s Profile`}
+          description="Elearning is a platform for students to learn and get help from teachers"
+          keywords="Programming, MERN, Redux, Machine Learning"
+        />
+
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+
+        <Profile />
+      </Protected>
+    </div>
+  );
+};
+
+export default page;
